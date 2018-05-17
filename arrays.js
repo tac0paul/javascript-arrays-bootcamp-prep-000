6c653dee8c295273b2723bae1ccd46b9cb075f96
paulo.gomes@owl.ucc.edu <paulo.gomes@owl.ucc.edu>
2018-05-17 00:52:22 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,4 +2,4 @@ var chocolateBars = [ 'snickers','hundred grand','kitkat','skittles' ];
   
   function addElementToBeginningOfArray(OGArray,addElement)
   {
-    OGArray = []
+    var OGArrays = ['OGArray','addElement']; }
