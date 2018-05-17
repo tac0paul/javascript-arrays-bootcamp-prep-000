c9a1a81b5205b133e48245188ef6b12913f5a47f
paulo.gomes@owl.ucc.edu <paulo.gomes@owl.ucc.edu>
2018-05-17 00:20:18 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,3 +4,20 @@ var ingredient3 = "sharp cheese"
 var ingredient4 = "butter"
 var ingredient5 = "tomato"
 var ingredient6 = "garlic"
+
+var grilledCheeseIngredients = [
+  'bread',
+  'mild cheese',
+  'sharp cheese',
+  'butter',
+  'tomato',
+  'garlic'
+]
+ 
+var tomatoSauceIngredients = [
+  'tomato',
+  'garlic',
+  'olive oil',
+  'basil',
+  'oregano'
+]
