f9910b1331f37ec57c7d34354d47bbfdcaa1c514
paulo.gomes@owl.ucc.edu <paulo.gomes@owl.ucc.edu>
2018-05-17 01:33:56 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,5 +4,5 @@ function addElementToBeginningOfArray(array,element) {
    return [element,...array]
 }
 function destructivelyAddElementToBeginningOfArray(array,element) {
-  return [element, array.unshift]
+  return [element,array.unshift]
 }
