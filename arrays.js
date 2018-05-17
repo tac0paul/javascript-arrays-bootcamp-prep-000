964cb451045e3dc6f9a3800d6fd3b488ed30c429
paulo.gomes@owl.ucc.edu <paulo.gomes@owl.ucc.edu>
2018-05-17 01:51:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -17,3 +17,7 @@ function destructivelyAddElementToEndOfArray(array,element) {
 }
 function accessElementInArray(array,index) {
 return array[index] }
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+array.shift(0)
+return array}
